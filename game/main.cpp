8a280b1ec03a1e047a6e80c934d84bebddb1d118
Nicolas Guillemot <nlguillemot@gmail.com>
2014-04-25 22:59:15 -0700

    implemented new SDL2plus interface to write skeleton code

--- game/main.cpp
+++ game/main.cpp
@@ -2,7 +2,31 @@
 
 void run()
 {
+    SDL2plus::LibSDL sdl(SDL_INIT_VIDEO);
+    sdl.SetGLAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    sdl.SetGLAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    sdl.SetGLAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
+    SDL2plus::WindowGL window("Beneath the Surface",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              640, 480, SDL_WINDOW_OPENGL);
+
+    while (true)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                goto MainLoopEnd;
+            }
+        }
+
+        window.SwapBuffers();
+    }
+
+    MainLoopEnd:;
 }
 
 int main(int argc, char* argv[])
