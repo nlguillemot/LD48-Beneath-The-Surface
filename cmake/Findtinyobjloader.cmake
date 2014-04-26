8653125d75b2e2e88d0a6accd9218ee3cf639ed7
Nicolas Guillemot <nlguillemot@gmail.com>
2014-04-25 21:34:02 -0700

    Squashed 'tinyobjloader/' content from commit 9d71172
    git-subtree-dir: tinyobjloader
    git-subtree-split: 9d71172e763addfa3ec7162c2aa0d580e675c478

---
+++ cmake/Findtinyobjloader.cmake
@@ -0,0 +1,8 @@
+# Defines:
+# tinyobjloader_FOUND - Always true
+# tinyobjloader_INCLUDE_DIR - Include directory of tinyobjloader
+# tinyobjloader_LIBRARY - the library for tinyobjloader
+
+set(tinyobjloader_FOUND TRUE)
+set(tinyobjloader_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../include)
+set(tinyobjloader_LIBRARY tinyobjloader)
