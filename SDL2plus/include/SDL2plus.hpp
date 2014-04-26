5b8452c3562af8a0f08cbea81d97b5b59e3f4e8b
Nicolas Guillemot <nlguillemot@gmail.com>
2014-04-25 22:50:31 -0700

    made SDL2plus more RAII and safe and decomposed and stuff

--- SDL2plus/include/SDL2plus.hpp
+++ SDL2plus/include/SDL2plus.hpp
@@ -15,37 +15,89 @@ public:
     LibSDL(Uint32 flags);
     ~LibSDL();
 
+    LibSDL(const LibSDL&) = delete;
+    LibSDL& operator=(const LibSDL&) = delete;
+    LibSDL(LibSDL&&) = delete;
+    LibSDL& operator=(LibSDL&&) = delete;
+
+    void InitSubSystem(Uint32 flags);
+    void QuitSubSystem(Uint32 flags);
+
     void SetGLAttribute(SDL_GLattr attr, int value);
 };
 
 class Window
 {
-    // TODO: fix this totally broken raii crap
-    struct WindowDeleter
-    {
-        void operator()(SDL_Window*) const;
-    };
-
-    std::unique_ptr<SDL_Window, WindowDeleter> mWindowHandle;
-
-    struct GLContextDeleter
-    {
-        void operator()(SDL_GLContext context) const;
-    };
-
-    std::unique_ptr<std::remove_pointer<SDL_GLContext>::type, GLContextDeleter> mGLContextHandle;
+    SDL_Window* mHandle = nullptr;
 
 public:
-    Window(int width, int height, const char* title, Uint32 flags = 0);
+    Window(const char* title, int x, int y, int w, int h, Uint32 flags);
+    ~Window();
+
+    Window(const Window&) = delete;
+    Window& operator=(const Window&) = delete;
+    Window(Window&& other) noexcept;
+    Window& operator=(Window&& other) noexcept;
+
+    void swap(Window& other) noexcept;
 
     void SetPosition(int x, int y);
 
     int GetWidth() const;
     int GetHeight() const;
 
-    void GLSwapWindow();
-
     SDL_Window* GetSDLHandle() const;
+};
+
+inline void swap(Window& a, Window& b) noexcept
+{
+    a.swap(b);
+}
+
+class GLContext
+{
+    SDL_GLContext mHandle = nullptr;
+
+public:
+    GLContext(Window& host);
+    ~GLContext();
+
+    GLContext(const GLContext&) = delete;
+    GLContext& operator=(const GLContext&) = delete;
+    GLContext(GLContext&& other) noexcept;
+    GLContext& operator=(GLContext&& other) noexcept;
+
+    void swap(GLContext& other) noexcept;
+
+    SDL_GLContext GetSDLHandle() const;
+};
+
+inline void swap(GLContext& a, GLContext& b) noexcept
+{
+    a.swap(b);
+}
+
+class WindowGL
+{
+    Window mWindow;
+    GLContext mGLContext;
+
+public:
+    WindowGL(const char* title, int x, int y, int w, int h, Uint32 flags);
+
+    void SetPosition(int x, int y) { mWindow.SetPosition(x,y); }
+
+    int GetWidth()  const { return mWindow.GetWidth();  }
+    int GetHeight() const { return mWindow.GetHeight(); }
+
+    void MakeCurrent();
+    void SwapBuffers();
+
+          Window& GetWindow()             { return mWindow;    }
+    const Window& GetWindow() const       { return mWindow;    }
+          GLContext& GetGLContext()       { return mGLContext; }
+    const GLContext& GetGLContext() const { return mGLContext; }
+
 };
 
 } // end namespace SDL2plus
--- SDL2plus/src/SDL2plus.cpp
+++ SDL2plus/src/SDL2plus.cpp
@@ -19,6 +19,19 @@ LibSDL::~LibSDL()
     SDL_Quit();
 }
 
+void LibSDL::InitSubSystem(Uint32 flags)
+{
+    if (SDL_InitSubSystem(flags))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
+void LibSDL::QuitSubSystem(Uint32 flags)
+{
+    SDL_QuitSubSystem(flags);
+}
+
 void LibSDL::SetGLAttribute(SDL_GLattr attr, int value)
 {
     if (SDL_GL_SetAttribute(attr, value))
@@ -27,82 +40,126 @@ void LibSDL::SetGLAttribute(SDL_GLattr attr, int value)
     }
 }
 
-void Window::WindowDeleter::operator()(SDL_Window* window) const
+Window::Window(const char* title, int x, int y, int w, int h, Uint32 flags)
 {
-    SDL_DestroyWindow(window);
-}
+    mHandle = SDL_CreateWindow(title, x, y, w, h, flags);
 
-void Window::GLContextDeleter::operator()(SDL_GLContext context) const
-{
-    SDL_GL_DeleteContext(context);
-}
-
-Window::Window(int width, int height, const char *title, Uint32 flags)
-{
-    mWindowHandle.reset(
-                SDL_CreateWindow(
-                    title,
-                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                    width, height, flags));
-
-    if (!mWindowHandle)
+    if (!mHandle)
     {
         throw std::runtime_error(SDL_GetError());
     }
+}
 
-    if (flags & SDL_WINDOW_OPENGL)
-    {
-        mGLContextHandle.reset(SDL_GL_CreateContext(mWindowHandle.get()));
-        if (!mGLContextHandle)
-        {
-            throw std::runtime_error(SDL_GetError());
-        }
+Window::~Window()
+{
+    SDL_DestroyWindow(mHandle);
+}
 
-        // wrangle GL extensions
-        glewExperimental = GL_TRUE;
-        GLenum glewError = glewInit();
-        if (glewError != GLEW_OK)
-        {
-            throw std::runtime_error((const char*) glewGetErrorString(glewError));
-        }
+Window::Window(Window&& other) noexcept
+{
+    swap(other);
+}
 
-        // flush errors. necessary because of http://stackoverflow.com/questions/19453439/solved-opengl-error-gl-invalid-enum-0x0500-while-glewinit 
-        while (glGetError() != GL_NO_ERROR);
-    }
+Window& Window::operator=(Window&& other) noexcept
+{
+    swap(other);
+}
+
+void Window::swap(Window& other) noexcept
+{
+    using std::swap;
+    swap(mHandle, other.mHandle);
 }
 
 void Window::SetPosition(int x, int y)
 {
-    SDL_SetWindowPosition(GetSDLHandle(), x, y);
+    SDL_SetWindowPosition(mHandle, x, y);
 }
 
 int Window::GetWidth() const
 {
     int w;
-    SDL_GetWindowSize(GetSDLHandle(), &w, NULL);
+    SDL_GetWindowSize(mHandle, &w, NULL);
     return w;
 }
 
 int Window::GetHeight() const
 {
     int h;
-    SDL_GetWindowSize(GetSDLHandle(), NULL, &h);
+    SDL_GetWindowSize(mHandle, NULL, &h);
     return h;
-}
-
-void Window::GLSwapWindow()
-{
-    if (!mGLContextHandle)
-    {
-        throw std::runtime_error("GLSwapWindow used on non-GL window.");
-    }
-
-    SDL_GL_SwapWindow(mWindowHandle.get());
 }
 
 SDL_Window* Window::GetSDLHandle() const
 {
-    return mWindowHandle.get();
+    return mHandle;
+}
+
+GLContext::GLContext(Window& host)
+{
+    mHandle = SDL_GL_CreateContext(host.GetSDLHandle());
+    if (!mHandle)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
+GLContext::~GLContext()
+{
+    SDL_GL_DeleteContext(mHandle);
+}
+
+GLContext::GLContext(GLContext&& other) noexcept
+{
+    swap(other);
+}
+
+GLContext& GLContext::operator=(GLContext&& other) noexcept
+{
+    swap(other);
+}
+
+void GLContext::swap(GLContext& other) noexcept
+{
+    using std::swap;
+    swap(mHandle, other.mHandle);
+}
+
+SDL_GLContext GLContext::GetSDLHandle() const
+{
+    return mHandle;
+}
+
+WindowGL::WindowGL(const char* title, int x, int y, int w, int h, Uint32 flags)
+    : mWindow(title, x, y, w, h,
+              (flags & SDL_WINDOW_OPENGL) ? flags
+                                          : throw std::logic_error(
+                                                "Missing SDL_WINDOW_OPENGL flag"))
+    , mGLContext(mWindow)
+{
+    // wrangle GL extensions
+    glewExperimental = GL_TRUE;
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK)
+    {
+        throw std::runtime_error((const char*) glewGetErrorString(glewError));
+    }
+
+    // flush errors. necessary because of http://stackoverflow.com/questions/19453439/solved-opengl-error-gl-invalid-enum-0x0500-while-glewinit
+    while (glGetError() != GL_NO_ERROR);
+}
+
+void WindowGL::MakeCurrent()
+{
+    if (SDL_GL_MakeCurrent(mWindow.GetSDLHandle(), mGLContext.GetSDLHandle()))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
+void WindowGL::SwapBuffers()
+{
+    SDL_GL_SwapWindow(mWindow.GetSDLHandle());
 }
 
 } // end namespace SDL2plus
